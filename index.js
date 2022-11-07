2a38a51ca614df4509142b332c4666a79d19c2eb
Roberto Lencione <92257405+rlencione@users.noreply.github.com>
2022-11-07 20:41:47 +0000

    07/11  md

--- index.js
+++ index.js
@@ -22,4 +22,183 @@ console.log(n);
 
 ________________________________________________________
 
-Level #2  variables aritimetics (video #8)*/
+Level #2 if/else statment (video #10)
+
+let myNote = 0
+
+if(myNote === 0){
+  myNote =+ 5;
+  console.log("Now I have,", myNote," notes!");
+} else {
+  myNote =+ 0;
+  console.log("Now I have,", myNote," notes!")
+}
+
+_________________________________________________________
+
+Level #2 if/else comparation (video #10)
+
+let num = 7;
+if(num > 7){
+  console.log("Greater than seven ")
+} else {
+  console.log("smaller than seven ")
+}
+
+_________________________________________________________
+
+Level #2 loop/while (video #16)
+
+let i = 0
+
+while(i < 10){
+  i++;
+  console.log(i);
+}
+
+________________________________________________________
+
+Level #2 loop/for (video #18)
+  
+for(let n = 0; n < 20; n++){
+  console.log(n);
+}
+
+for(let i = 10; i > 0; i--){
+  console.log(i);
+}
+
+_______________________________________________________
+
+Level #2 array (video #20)
+
+let arr = ['7AA','7AB','7AC','7AD','7AE','7AF'];
+console.log(arr);
+console.log(arr[1]);
+________________________________________________________
+
+Level #2 array/loop/while (video #22)
+
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+let len = arr.length;
+//console.log(arr);
+//console.log(len);
+
+for(let i = 0; i< len; i++){
+  if(arr[i] < 7){
+  console.log(arr[i]);
+}
+}
+________________________________________________________
+
+Level #3 loop's different cout (video #2)
+
+for(let i = 1; i < 10; i += 3 ){
+  console.log(i);
+}
+
+for(let i = 10; i > 0; i --){
+  console.log(i);
+}
+
+for(let i = 1; i < 200; i *= 3 ){
+  console.log(i);
+}
+
+for(let i = 3; i < 30000; (i**=2)){
+  console.log(i);
+}
+________________________________________________________
+
+Level #3 array operation (video #4)
+
+let arr = ['7AA','7AB','7AC','7AD','7AE','7AF'];
+console.log(arr);
+arr.push('8LA');
+console.log(arr);
+arr.pop();
+//let popped =arr.pop();
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.unshift('8LA');
+console.log(arr);
+arr.splice(2,1);
+console.log(arr);
+
+let acft = ['7AA','7AB','7AC','7AD','7AE','7AF'];
+console.log(acft);
+let sel = acft.splice(2,4);
+console.log(sel);
+________________________________________________________
+
+Level #3 logical operator (video #6)*/
+
+let opr = 55
+
+if(opr >15 && opr < 35){
+  let opc = 'passou'
+  console.log(opc);
+}  
+if(opr < 10 || opr > 15){
+  console.log(true);
+}
+
+
+
+
+
+
+
+
+
+
+
+/*Please answer our daily survey: https://airtable.com/shr4z93x5pfZr8Eke
+
+Ask Me Anything:
+https://microverse.zoom.us/webinar/register/4716504473343/WN_vDUMzCw3QHua9y4nhQtK3Q
+
+My LinkedIn profile:
+https://www.linkedin.com/in/ruben-d-pire/*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

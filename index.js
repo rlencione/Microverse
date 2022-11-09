b88847d48147836bc0a7b940520646c8f25675b6
Roberto Lencione <92257405+rlencione@users.noreply.github.com>
2022-11-09 01:50:25 +0000

    08/11

--- index.js
+++ index.js
@@ -1,138 +1,5 @@
-/*Level #2  variables aritimetics (video #8)
 
-let n = 1;
-console.log(n);
-
-n = n + 1;
-console.log(n);
-
-n += 1;
-console.log(n);
-
-n *= 1;
-console.log(n);
-
-n++;
-console.log(n);
-
-n++;
-n++;
-n++;
-console.log(n);
-
-________________________________________________________
-
-Level #2 if/else statment (video #10)
-
-let myNote = 0
-
-if(myNote === 0){
-  myNote =+ 5;
-  console.log("Now I have,", myNote," notes!");
-} else {
-  myNote =+ 0;
-  console.log("Now I have,", myNote," notes!")
-}
-
-_________________________________________________________
-
-Level #2 if/else comparation (video #10)
-
-let num = 7;
-if(num > 7){
-  console.log("Greater than seven ")
-} else {
-  console.log("smaller than seven ")
-}
-
-_________________________________________________________
-
-Level #2 loop/while (video #16)
-
-let i = 0
-
-while(i < 10){
-  i++;
-  console.log(i);
-}
-
-________________________________________________________
-
-Level #2 loop/for (video #18)
-  
-for(let n = 0; n < 20; n++){
-  console.log(n);
-}
-
-for(let i = 10; i > 0; i--){
-  console.log(i);
-}
-
-_______________________________________________________
-
-Level #2 array (video #20)
-
-let arr = ['7AA','7AB','7AC','7AD','7AE','7AF'];
-console.log(arr);
-console.log(arr[1]);
-________________________________________________________
-
-Level #2 array/loop/while (video #22)
-
-let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
-let len = arr.length;
-//console.log(arr);
-//console.log(len);
-
-for(let i = 0; i< len; i++){
-  if(arr[i] < 7){
-  console.log(arr[i]);
-}
-}
-________________________________________________________
-
-Level #3 loop's different cout (video #2)
-
-for(let i = 1; i < 10; i += 3 ){
-  console.log(i);
-}
-
-for(let i = 10; i > 0; i --){
-  console.log(i);
-}
-
-for(let i = 1; i < 200; i *= 3 ){
-  console.log(i);
-}
-
-for(let i = 3; i < 30000; (i**=2)){
-  console.log(i);
-}
-________________________________________________________
-
-Level #3 array operation (video #4)
-
-let arr = ['7AA','7AB','7AC','7AD','7AE','7AF'];
-console.log(arr);
-arr.push('8LA');
-console.log(arr);
-arr.pop();
-//let popped =arr.pop();
-console.log(arr);
-arr.shift();
-console.log(arr);
-arr.unshift('8LA');
-console.log(arr);
-arr.splice(2,1);
-console.log(arr);
-
-let acft = ['7AA','7AB','7AC','7AD','7AE','7AF'];
-console.log(acft);
-let sel = acft.splice(2,4);
-console.log(sel);
-________________________________________________________
-
-Level #3 logical operator (video #6)*/
+/*Level #3 logical operator (video #6)
 
 let opr = 55
 
@@ -143,48 +10,83 @@ if(opr >15 && opr < 35){
 if(opr < 10 || opr > 15){
   console.log(true);
 }
+_________________________________________________________
 
+/*Level #3 Sting trunkation (video #9)
 
+let world ='Nationality';
+console.log(world.substring(3));
 
+let cuted = world.substring(5);
+console.log(cuted);
+_________________________________________________________
 
+/*Level #3 Found values in array (video #11)
 
+let users = ['7AA','7AB','7AC','7AD','7AE','7AF'];
+let mec = '8LA';
 
+if(users.includes(mec)===true){
+  console.log('Included')
+} else {
+  console.log('Not include')
+}
+_________________________________________________________
 
+Level #3 Constricting array (video #11)
 
+let time = [];
 
+for(let i = 1; i < 12; i++){
+  time.push(i);
+}
+//console.log(time);
 
+let par = [];
+for(let i = 1; i < time.length; i++){
+  if(time[i] > 5){
+  par.push(time[i]);
+}
+}
+console.log(time);
+console.log(par);
+____________________________________________________________
 
-/*Please answer our daily survey: https://airtable.com/shr4z93x5pfZr8Eke
+Level #3 Filtering array whit loops(video #13)
 
-Ask Me Anything:
-https://microverse.zoom.us/webinar/register/4716504473343/WN_vDUMzCw3QHua9y4nhQtK3Q
+let get = [10, 55, 6, 84, 12, 24,5 ,9]
+let com = [];
 
-My LinkedIn profile:
-https://www.linkedin.com/in/ruben-d-pire/*/
+for(let i = 0; i < 10; i++){
+  if(get[i] > 5){
+  com.push(get[i]);
+  }
+}
+console.log(com);
+____________________________________________________________
 
+Level #3 acessing arrays whit subarrrays (video #15)
 
+let major = [[1,2,3],[4,5,6],[8,7,4],[9,9,9,9,9]];
+let biger = []
 
+for(let i = 0; i < major.length; i++){
+  console.log(major[i]);
+for(let n = 0; n < (major[i]).length; n++)
+  console.log(major[i][n]);
+}
 
+console.log(major);
+console.log(major[2]);
+console.log(biger);
+______________________________________________________________________
 
+Level #3 Constrcting arrays whit subarrrays (video #17)*/
 
+let biger = [];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for(let j = 0; j < 3; j++){
+  
 
 
 

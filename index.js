a9c82d99fa485ac90f8885ce3db3eea3a470406b
Roberto Lencione <92257405+rlencione@users.noreply.github.com>
2022-11-09 03:38:09 +0000

    08/11_2

--- index.js
+++ index.js
@@ -1,103 +1,58 @@
-
-/*Level #3 logical operator (video #6)
-
-let opr = 55
-
-if(opr >15 && opr < 35){
-  let opc = 'passou'
-  console.log(opc);
-}  
-if(opr < 10 || opr > 15){
-  console.log(true);
-}
-_________________________________________________________
-
-/*Level #3 Sting trunkation (video #9)
-
-let world ='Nationality';
-console.log(world.substring(3));
-
-let cuted = world.substring(5);
-console.log(cuted);
-_________________________________________________________
-
-/*Level #3 Found values in array (video #11)
-
-let users = ['7AA','7AB','7AC','7AD','7AE','7AF'];
-let mec = '8LA';
-
-if(users.includes(mec)===true){
-  console.log('Included')
-} else {
-  console.log('Not include')
-}
-_________________________________________________________
-
-Level #3 Constricting array (video #11)
-
-let time = [];
-
-for(let i = 1; i < 12; i++){
-  time.push(i);
-}
-//console.log(time);
-
-let par = [];
-for(let i = 1; i < time.length; i++){
-  if(time[i] > 5){
-  par.push(time[i]);
-}
-}
-console.log(time);
-console.log(par);
-____________________________________________________________
-
-Level #3 Filtering array whit loops(video #13)
-
-let get = [10, 55, 6, 84, 12, 24,5 ,9]
-let com = [];
-
-for(let i = 0; i < 10; i++){
-  if(get[i] > 5){
-  com.push(get[i]);
-  }
-}
-console.log(com);
-____________________________________________________________
-
-Level #3 acessing arrays whit subarrrays (video #15)
-
-let major = [[1,2,3],[4,5,6],[8,7,4],[9,9,9,9,9]];
-let biger = []
-
-for(let i = 0; i < major.length; i++){
-  console.log(major[i]);
-for(let n = 0; n < (major[i]).length; n++)
-  console.log(major[i][n]);
-}
-
-console.log(major);
-console.log(major[2]);
-console.log(biger);
-______________________________________________________________________
-
-Level #3 Constrcting arrays whit subarrrays (video #17)*/
+/*Level #3 Constrcting arrays whit subarrrays (video #17)
 
 let biger = [];
-
-for(let j = 0; j < 3; j++){
+let extctr = 1
+for(let i = 0; i < 3; i++){
+  let little = [];
+  for(let j = 0; j < 3; j++){
+    little.push(extctr);
+    extctr++;
+    console.log(little);
+  }
+  biger.push(little);
+}
+____________________________________________________________
   
+Level #3 functions (video #19)
 
+function somar(){
+  let x = 3;
+  let y = 5;
+  console.log(x+y);
+}
+somar();
 
+function sum(){
+  let a=2;
+  let b=5;
+  return a+b;
+}
+let ret = sum();
+console.log(ret)
 
+function soma(a, b){
+  console.log(a+b);
+}
+soma(8, 18);
+soma(2, 9);
 
+function juice(ingre_1, ingre_2, ingre_3){
+  console.log("My juice has,", ingre_1,",", ingre_2, "and", ingre_3, ",Nice juice!")
+}
+juice('laranja', 'maçã', 'pera');
+juice('laranja', 'pera');
+______________________________________________________________
 
+Level #3 Return functions output (video #21)*/
 
+function ret(k, f){
+  return k * f;
+}
+let result = ret(20, 10);
+let moreresult = ret(20, 10) + ret(10, 10);
 
-
-
-
-
+console.log(result);
+console.log(moreresult);
 
 
 

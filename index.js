f6535951689d663520030bd14f754a190061a122
Roberto Lencione <92257405+rlencione@users.noreply.github.com>
2022-11-15 13:01:53 +0000

    Level #4 Truncate a string (Video 17)

--- index.js
+++ index.js
@@ -1,147 +1,19 @@
-/*Level #3 Constrcting arrays whit subarrrays (video #17)
+/*Level #4 Truncate a string (Video 17)*/
 
-let biger = [];
-let extctr = 1
-for(let i = 0; i < 3; i++){
-  let little = [];
-  for(let j = 0; j < 3; j++){
-    little.push(extctr);
-    extctr++;
-    console.log(little);
+
+function truncateString(str, num) {
+  let trunk = str.substring(0, num);
+  if(num < str.length){
+    trunk +="...";
+    return trunk;
+  }else{
+    return str;
   }
-  biger.push(little);
-}
-____________________________________________________________
   
-Level #3 functions (video #19)
-
-function somar(){
-  let x = 3;
-  let y = 5;
-  console.log(x+y);
 }
-somar();
-
-function sum(){
-  let a=2;
-  let b=5;
-  return a+b;
-}
-let ret = sum();
-console.log(ret)
-
-function soma(a, b){
-  console.log(a+b);
-}
-soma(8, 18);
-soma(2, 9);
-
-function juice(ingre_1, ingre_2, ingre_3){
-  console.log("My juice has,", ingre_1,",", ingre_2, "and", ingre_3, ",Nice juice!")
-}
-juice('laranja', 'maçã', 'pera');
-juice('laranja', 'pera');
-______________________________________________________________
-
-Level #3 Return functions output (video #21)
-
-function ret(k, f){
-  return k * f;
-}
-let result = ret(20, 10);
-let moreresult = ret(20, 10) + ret(10, 10);
+  //for(let i = 1; i < str.length; i ++);
+    
+let result = truncateString("A-tisket a-tasket A green and yellow basket", 8);
 
 console.log(result);
-console.log(moreresult);
-_____________________________________________________________
-
-Level #3 Reverse and translate arrays (video #23)
-
-function reverse(arr){
-  let output = [];
-  for(let i = arr.length -1; i >= 0; i--){
-    output.push(arr[i]);
-}
-    return output;
-}
-
-let result = reverse([1,2,3,4,5,6,9,7,8]);
-console.log(result);
-
-function binary(ary){
-  let output = [];
-   for(let i = 0; i < ary.length; i++){
-     if(ary[1] === 1){
-       output.push('one');
-     }
-     if(ary[i] === 0){
-       output.push('zero');
-    }
-   } 
-   return output
-}
-
-let ring = binary([0,1,1,0,0,1]);
-console.log(ring);
-______________________________________________________________
-
-Level #4 Reserse string (video #09)
-
-function reverse(str){
-  let concat =[];
-  let back ="";
-  //for(let i = str.length -1; i >= 0; i --){
-  let i = str.length;
-    while(i >= 0){
-    i--;
-    back += str[i]
-    }
- return back; 
-
-}  
-let result = reverse("consulta");
-console.log(result);
-_____________________________________________________________
-
-Level #4 Factorialize a number (video #13)
-
-function factorialize(num){
- let calc = 1;
- for (let i = 1; i <= num; i++){
-  calc *= i;
- }
-  console.log(calc);
-}
-factorialize(20);
--------------------------------------------------------------
-Level #4 truncate a string (video #17)*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 

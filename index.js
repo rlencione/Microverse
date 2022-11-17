d05bac7d3988ded0f208acfb4b706d18ab246a18
Roberto Lencione <92257405+rlencione@users.noreply.github.com>
2022-11-17 21:19:47 +0000

    Level #4 splits array (Video 24)

--- index.js
+++ index.js
@@ -1,19 +1,80 @@
-/*Level #4 Truncate a string (Video 17)*/
+/*Level #4 Where do I belong (Video 21)
 
-
-function truncateString(str, num) {
-  let trunk = str.substring(0, num);
-  if(num < str.length){
-    trunk +="...";
-    return trunk;
-  }else{
-    return str;
-  }
-  
+function getIndexToIns(arr, num) {
+  let blong = 0;
+      for(let i = 0; i < arr.length; i++){
+      if(arr[i] < num ){
+        blong ++;
+      console.log(blong);
+      }
+      }
+  return blong;
 }
-  //for(let i = 1; i < str.length; i ++);
-    
-let result = truncateString("A-tisket a-tasket A green and yellow basket", 8);
 
-console.log(result);
+getIndexToIns([40, 60], 50);
+___________________________________________________________
+Level #4 splits array (Video 24)
+
+function chunkArrayInGroups(arr, size) {
+  let spl = [];
+  let min = [];
+  let max = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(max === 0);{
+    max = 0;
+    min = [];
+  }
+    min.push(arr[i]);
+    console.log(max, min);
+    max ++;
+  }
+return arr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+function chunk(arr, size){
+  let spl = [];
+  let max = [];
+  let min = 0;
+  for (let i = 0; i < arr.length; i++){
+    if(min === size){
+      min = 0;
+      spl.push(max);
+      max = [];
+    }
+    max.push(arr[i])
+      
+      min ++;
+  }
+  spl.push(max);
+  console.log(spl);
+  return arr;
+}
+
+chunk(["a", "b", "c", "d", "e", "f", "g", "h", "i"], 3);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
